e664007cce5acdf759be2b6fd06b108d5ea8d713
Mathias <mathias.grandin@gmail.com>
2020-07-15 15:31:31 +0200

    Esercizio quasi completo

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// Chiedere all'utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// parola utente
+var wordUser = prompt("Inserisci una parola");
+
+
+// funzione su parola utente
+reverse(wordUser);
+
+// funzione
+function reverse(word) {
+  var wordReverse = '';
+// ciclo per invertire parola
+  for (var i = word.length - 1 ; i >= 0; i--) {
+    wordReverse += word[i];
+  }
+// controllo della parola
+  if (word == wordReverse) {
+    alert("Congratulazioni! La tua mente brillante ha partorito un palindromo!");
+    console.log("Palindromo");
+  } else {
+    alert("Bella parola! Ma non è quella che cerchiamo!");
+    console.log("Non è un palindromo");
+  }
+}
